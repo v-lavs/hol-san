017f4da465b650ef1b23fba7bb634f932eb1938e
v-lavs <lavskavanda@gmail.com>
2024-03-03 17:54:22 +0200

    11

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,8 @@
 
 $(document).ready(function () {
     const lenis = new Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(6, 1.001 - Math.pow(2, -10 * t)),
+        duration: 2.5,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     })
 
     function raf(time) {
@@ -246,6 +246,7 @@ $(document).ready(function () {
                 pin: true,
                 duration: 3,
                 scrub: 6,
+
                 invalidateOnRefresh: true,
                 // markers: true
             }
@@ -326,6 +327,38 @@ $(document).ready(function () {
 
     const symptoms = symptomsAnim();
 
+    // function imgScaleAnim() {
+    //     const imgTimeline = gsap.timeline({
+    //         scrollTrigger: {
+    //             trigger: '#about-main-img-start',
+    //             start: 'top center',
+    //             end: 'bottom center ',
+    //             scrub: true,
+    //             markers: true,
+    //         }
+    //     });
+    //
+    //     const details = document.querySelector('#about-main-img-end img');
+    //     Flip.fit(details, document.querySelector('#about-main-img-start'), {
+    //         scale: true,
+    //         fitChild: document.querySelector('#about-main-img-end img')
+    //     });
+    //
+    //     const state = Flip.getState(details);
+    //
+    //     // set the final state
+    //     gsap.set(details, {clearProps: true}); // wipe out all inline stuff so it's in the native state (not scaled)
+    //     gsap.set(details, {visibility: "visible", overflow: "hidden"});
+    //
+    //     Flip.from(state, {
+    //         duration: 1.4,
+    //         ease: "power2.inOut",
+    //         scale: true,
+    //     })
+    //     imgTimeline.to('#about-main-img-start img', {yPercent: 45});
+    //     return imgScaleAnim();
+    // }
+    // const imgScale = imgScaleAnim();
 
     function aboutAnim() {
         const timeline = gsap.timeline({
@@ -409,389 +442,7 @@ $(document).ready(function () {
         symptoms.refresh();
         effIntro.refresh();
         about.refresh();
-
     });
-    //
-    // const sections = gsap.utils.toArray(".section .dark");
-    // const dickl = document.getElementsByClassName("disclaimer-main");
-    //
-    // function changeColor(sec) {
-    //     if (getComputedStyle(sec).backgroundColor === "rgb(255, 255, 255)") {
-    //         dickl.classList.add("color-change");
-    //
-    //     } else {
-    //         dickl.classList.remove("color-change");
-    //     }
-    // }
-    //
-    // sections.forEach((section) => {
-    //     ScrollTrigger.create({
-    //         trigger: section,
-    //         start: "bottom top",
-    //         end: "bottom bottom",
-    //         onEnter: () => changeColor(section),
-    //         onEnterBack: () => changeColor(section),
-    //     });
-    // });
-    //
-    // //--------------------------------------------SECT BANNER ------------------------
-    //
-    // // Banner letters
-    // const bannerTitle = document.querySelector('.section-banner .section__title');
-    // if (bannerTitle) {
-    //     const text = bannerTitle.innerText;
-    //     const htmlText = text.split('').map(letter => {
-    //         if (letter.trim()) {
-    //             return '<span class="section-banner__title-letter">' + letter + '</span>';
-    //         }
-    //         return letter;
-    //     }).join('');
-    //
-    //     bannerTitle.innerHTML = htmlText;
-    //     const chars = document.querySelectorAll('.section-banner__title-letter');
-    //
-    //     if (chars) {
-    //         gsap.fromTo(chars, {opacity: 0.6}, {
-    //             stagger: 0.075,
-    //             ease: 'back.in',
-    //             duration: 0.01,
-    //             opacity: 1,
-    //         })
-    //     }
-    // }
-    //
-    // const bannerImg1 = document.querySelector('.img-banner_1');
-    // const bannerImg2 = document.querySelector('.img-banner_2');
-    // const disclaimerMain = document.querySelector('.disclaimer-main');
-    //
-    // if (bannerImg1) {
-    //     gsap.fromTo(bannerImg1, {
-    //         x: 500,
-    //         duration: -1,
-    //         opacity: 0
-    //     }, {
-    //         x: 0,
-    //         opacity: 1,
-    //         stagger: 0.2,
-    //         ease: 'power1.out',
-    //         duration: 1.2,
-    //         delay: 0.2
-    //     })
-    // }
-    // if (bannerImg2) {
-    //     gsap.fromTo(bannerImg2, {
-    //         x: 500,
-    //         duration: -1,
-    //         opacity: 0
-    //     }, {
-    //         x: 0,
-    //         opacity: 1,
-    //         stagger: 0.2,
-    //         ease: 'power.out',
-    //         duration: 1.1,
-    //         delay: 0.35
-    //     })
-    // }
-    // if (disclaimerMain) {
-    //     gsap.fromTo(disclaimerMain, {
-    //         y: 120,
-    //         duration: -1,
-    //         opacity: 0
-    //     }, {
-    //         y: 0,
-    //         opacity: 1,
-    //         stagger: 0.2,
-    //         ease: 'power1.out',
-    //         duration: 1.2,
-    //         delay: 0.25
-    //     });
-    // }
-    //
-    // const banner = document.querySelector('.section-banner');
-    // const bannerContainer = document.querySelector('.section-banner .container');
-    // const bannerGoOutStage = gsap.to(
-    //     bannerContainer, {
-    //         opacity: 0,
-    //         yPercent: -100,
-    //     })
-    //
-    // ScrollTrigger.create({
-    //     animation: bannerGoOutStage,
-    //     trigger: document.querySelector('.section-effects'),
-    //     end: "bottom center",
-    //     scrub: 0.1,
-    //     immediateRender: true,
-    // });
-    //
-    // ScrollTrigger.create({
-    //     trigger: banner,
-    //     start: () => banner.offsetHeight < window.innerHeight ? "top top" : "bottom bottom", // if it's shorter than the viewport, we prefer to pin it at the top
-    //     pin: true,
-    //     pinSpacing: false
-    // });
-    //
-    // //----------------------------------------SECT EFFECT---------------
-    //
-    // const sectionEffects = document.querySelector('.section-effects');
-    // ScrollTrigger.create({
-    //     trigger: sectionEffects,
-    //     start: 'top top', // if it's shorter than the viewport, we prefer to pin it at the top
-    //     pin: true,
-    //     scrub: true,
-    //     end: "bottom+=200% 200%",
-    // });
-    // gsap.to('.section-effects .card__thumb',
-    //     {
-    //         yPercent: 50,
-    //         pin: true,
-    //         scrollTrigger: {
-    //             trigger: ".section-effects",
-    //             start: 'top top',
-    //             end: 'bottom center',
-    //             scrub: 3,
-    //         }
-    //     })
-    // gsap.to(".section-effects .card__text",
-    //     {
-    //         opacity: 0,
-    //         scrollTrigger: {
-    //             trigger: ".section-effects",
-    //             start: 'top top',
-    //             end: 'bottom center',
-    //             scrub: 3,
-    //         }
-    //     }, "-=1");
-    //
-    // gsap.to('.section-effects .effects',
-    //     {
-    //         scale: 0.45,
-    //         opacity: 0,
-    //         delay: 2.5,
-    //         ease: 'circ.inOut',
-    //         scrollTrigger: {
-    //             trigger: ".section-effects",
-    //             scrub: 3,
-    //             start: "center 70%",
-    //             end: 'bottom top',
-    //
-    //         }
-    //     },
-    // )
-    // gsap.to('.section-effects .effects__pill',
-    //     {
-    //         x: 0,
-    //         ease: 'circ.inOut',
-    //         scrollTrigger: {
-    //             trigger: ".section-effects",
-    //             scrub: 3,
-    //             start: "center 70%",
-    //             end: 'bottom top',
-    //         }
-    //     },
-    // )
-    // gsap.to('.section-effects .pill-side',
-    //     {
-    //         width: 0,
-    //         ease: 'circ.inOut',
-    //         scrollTrigger: {
-    //             trigger: ".section-effects",
-    //             scrub: 3,
-    //             start: "center 70%",
-    //             end: 'bottom top',
-    //         }
-    //     }
-    // )
-    // ScrollTrigger.refresh();
-    //
-    // // -------------------------------------SEC_SYMPT---------------------
-    //
-    // const sectionSym = document.querySelector('.section-symptoms');
-    // ScrollTrigger.create({
-    //     trigger: sectionSym,
-    //     start: 'top top',
-    //     pin: true,
-    //     scrub: true,
-    //     end: "bottom+=200% 200%",
-    // });
-    // gsap.fromTo(' .img_1',
-    //     {
-    //         y: 100,
-    //         opacity: 0,
-    //     },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         ease: 'power1.out',
-    //
-    //         scrollTrigger: {
-    //             trigger: ".section-symptoms",
-    //             scrub: 3,
-    //             start: "top 35%",
-    //             end: 'bottom bottom'
-    //         },
-    //     },
-    // )
-    // gsap.fromTo('.block-bg',
-    //     {
-    //         y: 100,
-    //         opacity: 0,
-    //     },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         stagger: 0.2,
-    //         ease: 'power1.out',
-    //
-    //         scrollTrigger: {
-    //             trigger: ".section-symptoms",
-    //             scrub: 3,
-    //             start: "top 25%",
-    //             end: 'bottom bottom'
-    //         },
-    //     },
-    // )
-    //
-    // gsap.fromTo('.img-ellipse_warm',
-    //     {
-    //         y: 100,
-    //         opacity: 0,
-    //         // duration: -1,
-    //     },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         stagger: 0.1,
-    //         ease: 'power1.out',
-    //         scrollTrigger: {
-    //             trigger: ".section-symptoms",
-    //             scrub: 3,
-    //             start: "top 15%",
-    //             end: 'bottom bottom'
-    //         },
-    //
-    //     },
-    // )
-    // gsap.fromTo('.img-ellipse_green',
-    //     {
-    //         y: 100,
-    //         opacity: 0,
-    //         // duration: -1,
-    //     },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         // stagger: 0.1,
-    //         ease: 'power1.out',
-    //         scrollTrigger: {
-    //             trigger: ".section-symptoms",
-    //             scrub: 3,
-    //             start: "top 18%",
-    //             end: 'bottom bottom',
-    //         },
-    //     },
-    // )
-    //
-    // gsap.fromTo('.section-symptoms .section__title',
-    //     {
-    //         y: 100,
-    //         opacity: 0,
-    //         duration: -1,
-    //     },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         stagger: 0.4,
-    //         ease: 'power1.out',
-    //         duration: 2.2,
-    //         scrollTrigger: {
-    //             trigger: ".section-symptoms",
-    //             scrub: 3,
-    //             start: "top 20%",
-    //             end: 'top bottom',
-    //         }
-    //     })
-    // gsap.fromTo('.dot',
-    //     {
-    //         y: 50,
-    //         opacity: 0,
-    //         duration: -1,
-    //     },
-    //     {
-    //         y: 0,
-    //         opacity: 1,
-    //         stagger: 0.4,
-    //         ease: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    //         duration: 1.2,
-    //         scrollTrigger: {
-    //             trigger: ".section-symptoms",
-    //             scrub: 3,
-    //             start: "top 45%",
-    //             end: 'top bottom',
-    //         }
-    //     })
-    // document.querySelectorAll(".symptoms .dot").forEach(function (box) {
-    //     gsap.fromTo(".symptoms .dot",
-    //         {
-    //             opacity: 0,
-    //             duration: -1,
-    //         },
-    //         {
-    //             opacity: 1,
-    //             stagger: 0.4,
-    //             ease: 'power1.in',
-    //             scrollTrigger: {
-    //                 trigger: ".section-symptoms",
-    //                 scrub: 3,
-    //                 start: "bottom top",
-    //                 end: 'bottom bottom'
-    //             },
-    //         },);
-    // });
-    // document.querySelectorAll(".symptoms .card").forEach(function (box) {
-    //     gsap.fromTo(".symptoms .card",
-    //         {
-    //             opacity: 0,
-    //             duration: -1,
-    //         },
-    //         {
-    //             opacity: 1,
-    //             stagger: 0.2,
-    //             ease: 'power1.in',
-    //             scrollTrigger: {
-    //                 trigger: ".section-symptoms",
-    //                 scrub: 3,
-    //                 start: "top 20%",
-    //                 end: 'bottom bottom-=50'
-    //             },
-    //         },);
-    // });
-    //
-    //
-    // ScrollTrigger.refresh();
-    //
-    //
-    // // -------------------------------------FLIP------------------------------------
-    // gsap.registerPlugin(Flip);
-    //
-    //
-    // const details = document.querySelector('#about-main-img-end img');
-    // Flip.fit(details, document.querySelector('#about-main-img-start'), {
-    //     scale: true,
-    //     fitChild: document.querySelector('#about-main-img-end img')
-    // });
-    //
-    // const state = Flip.getState(details);
-    //
-    // // set the final state
-    // gsap.set(details, {clearProps: true}); // wipe out all inline stuff so it's in the native state (not scaled)
-    // gsap.set(details, {visibility: "visible", overflow: "hidden"});
-    //
-    // Flip.from(state, {
-    //     duration: 1.4,
-    //     ease: "power2.inOut",
-    //     scale: true,
-    // })
-
 
 });
 
