4c94fc0fb30bd8396ca05d77aad72042efda9322
v-lavs <lavskavanda@gmail.com>
2024-03-05 17:39:51 +0200

    bnt_buy, btn_up

--- src/js/main.js
+++ src/js/main.js
@@ -126,6 +126,34 @@ $(document).ready(function () {
             });
         });
 
+
+    (function() {
+
+        function trackScroll() {
+            var scrolled = window.pageYOffset;
+            var coords = document.documentElement.clientHeight;
+
+            if (scrolled > coords) {
+                goTopBtn.classList.add('show');
+            }
+            if (scrolled < coords) {
+                goTopBtn.classList.remove('show');
+            }
+        }
+
+        function backToTop() {
+            if (window.pageYOffset > 0) {
+                window.scrollBy(0, -80);
+                setTimeout(backToTop, 0);
+            }
+        }
+
+        var goTopBtn = document.querySelector('.btn_up');
+
+        window.addEventListener('scroll', trackScroll);
+        goTopBtn.addEventListener('click', backToTop);
+    })();
+
         // ------------------------------------------GSAP----------------------
 
         gsap.registerPlugin(ScrollTrigger, Flip);
